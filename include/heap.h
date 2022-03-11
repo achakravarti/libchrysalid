6a57188f1c585ccddb984152732963843b099407
Abhishek Chakravarti <abhishek@taranjali.org>
2022-03-11 12:12:31 +0530

    Declare adapter and factory functions
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/heap.h
+++ include/heap.h
@@ -13,7 +13,7 @@ extern "C" {
 enum c_cmp { C_CMP_LT = -1, C_CMP_EQ, C_CMP_GT };
 
 
-typedef struct c_heap_ c_heap;
+typedef void c_heap;
 
 
 extern c_heap *
@@ -65,28 +65,55 @@ str(const c_heap *);
 
 
 
-struct c_heap_ {
-        void                            *bfr;
-        const struct c_heap_vtable_     *vtable;
-        void                             (*free_cbk)(struct c_heap_ *);
-};
+
+typedef void (c_heap_freecbk)(c_heap *);
 
 
 struct c_heap_vtable_ {
-        c_heap          *(*copy)(const c_heap *);
+        //c_heap          *(*copy)(const c_heap *);
         c_heap          *(*clone)(const c_heap *);
         c_heap          *(*clone_aligned)(const c_heap *, size_t);
-        void             (*free)(c_heap **);
+        c_heap_freecbk *free_cbk;
         enum c_cmp       (*cmp)(const c_heap *, c_heap *);
-        size_t           (*sz)(const c_heap *);
+        //size_t           (*sz)(const c_heap *);
         size_t           (*sz_total)(const c_heap *);
-        size_t           (*refc)(const c_heap *);
-        bool             (*aligned)(const c_heap *, size_t);
+        //size_t           (*refc)(const c_heap *);
+        //bool             (*aligned)(const c_heap *, size_t);
         void             (*resize)(c_heap **, size_t);
         void             (*resize_aligned)(c_heap **, size_t, size_t);
         const char      *(*str)(const c_heap *);
 };
 
+
+extern size_t *
+c_heap_head_(const c_heap *);
+
+
+extern const struct c_heap_vtable_ *
+c_heap_vtable_(const c_heap *);
+
+extern void
+c_heap_vtable_set_(c_heap *, const struct c_heap_vtable_ *);
+
+
+extern void
+c_heap_sz_set_(c_heap *, size_t);
+
+extern void
+c_heap_refc_set_(c_heap *, size_t);
+
+
+c_heap *
+c_heap_std_new(size_t, size_t);
+
+c_heap *
+c_heap_std_new_freecbk(size_t, size_t, c_heap_freecbk *);
+
+c_heap *
+c_heap_std_new_aligned(size_t, size_t, size_t);
+
+c_heap *
+c_heap_std_new_aligned_freecbk(size_t, size_t, size_t, c_heap_freecbk *);
 
 
 
