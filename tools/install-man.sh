bb44a0593cdc2cf6e1a8d7552efdb4795aebc94f
Abhishek Chakravarti <abhishek@taranjali.org>
2022-03-18 21:22:24 +0530

    Install components documentation
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/install-man.sh
+++ tools/install-man.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
 
-sudo mkdir -p /usr/local/man/man3
-sudo rm -f /usr/local/man/man3/libchrysalis:*
+# Ensure man page subdirectories exist
+sudo mkdir -p /usr/local/share/man/man3
+sudo mkdir -p /usr/local/share/man/man7
 
 
+# Remove previous man page documentation
+sudo rm -f /usr/local/share/man/man3/libchrysalis:*
+sudo rm -f /usr/local/share/man/man7/libchrysalis:*
+
+
+# Find man page documentation
 __man3=$(find build/docs/ -type f -name "*.3.gz")
+__man7=$(find build/docs/ -type f -name "*.7.gz")
 
+
+# Install man 3 documentation
 for x in $__man3; do 
-	sudo cp "$x" /usr/local/man/man3
+	sudo cp "$x" /usr/local/share/man/man3
 done
 
+
+# Install man 7 documentation
+for x in $__man7; do 
+	sudo cp "$x" /usr/local/share/man/man7
+done
+
+
+# Update manpage database
 sudo mandb
 
