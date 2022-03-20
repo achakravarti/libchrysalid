8c4aaed9ee80b83401ea27d88b2d701f06cbe484
Abhishek Chakravarti <abhishek@taranjali.org>
2022-03-20 11:04:32 +0530

    Account for varying man page root locations
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/install-man.sh
+++ tools/install-man.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
+# Determing root directory for man pages; on Linux FHS it is /usr/local/man and
+# in FreeBSD it is /usr/local/share/man
+if [ "$(uname)" = "Linux" ]; then
+	__man="/usr/local/man"
+elif [ "$(uname)" = "FreeBSD" ]; then
+	__man="/usr/local/share/man"
+else
+	echo "Unsupported OS; exiting..."
+	exit
+fi
+
 
 # Ensure man page subdirectories exist
-sudo mkdir -p /usr/local/share/man/man3
-sudo mkdir -p /usr/local/share/man/man7
+sudo mkdir -p $__man/man3
+sudo mkdir -p $__man/man7
 
 
-# Remove previous man page documentation
-sudo rm -f /usr/local/share/man/man3/libchrysalis:*
-sudo rm -f /usr/local/share/man/man7/libchrysalis:*
+# Remove previous man paguxe documentation
+sudo rm -f $__man/man3/libchrysalis:*
+sudo rm -f $__man/man7/libchrysalis:*
 
 
 # Find man page documentation
@@ -18,13 +29,13 @@ __man7=$(find build/docs/ -type f -name "*.7.gz")
 
 # Install man 3 documentation
 for x in $__man3; do 
-	sudo cp "$x" /usr/local/share/man/man3
+	sudo cp "$x" $__man/man3
 done
 
 
 # Install man 7 documentation
 for x in $__man7; do 
-	sudo cp "$x" /usr/local/share/man/man7
+	sudo cp "$x" $__man/man7
 done
 
 
