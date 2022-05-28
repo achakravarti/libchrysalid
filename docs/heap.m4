655c78e3208763eb9d82e70591b16463d6da54b0
Abhishek Chakravarti <abhishek@taranjali.org>
2022-05-28 13:27:39 +0530

    Generate heap man page
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ docs/heap.m4
@@ -0,0 +1,9 @@
+include(`macros.m4')
+
+
+ifdef(<<<MAN>>>,
+<<<
+% LIBCHRYSALID:HEAP(3) libchrysalid 0.0.1
+%
+% 2022
+>>>)
---
+++ tools/mandoc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+tools/extract.sh
+
+mkdir -p build/docs/man
+
+m4 -DMAN -I docs/ -I build/docs/ docs/heap.m4 > "build/docs/man/heap.md"
+
+# Remove blank lines from top of markdown file
+sed -i '/./,$!d' "build/docs/man/heap.md"
+
+pandoc "build/docs/man/heap.md" \
+    --bibliography=docs/references.bib \
+    --citeproc \
+    -s \
+    -t man \
+    -o "build/docs/man/libchrysalid:heap.3"
+
+gzip -f "build/docs/man/libchrysalid:heap.3"
