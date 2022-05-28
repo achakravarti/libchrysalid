a3b070296261cb9a8129e574fd2be13fa1e50400
Abhishek Chakravarti <abhishek@taranjali.org>
2022-05-28 12:49:35 +0530

    Expand documentation macros
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/extract.sh
+++ tools/extract.sh
@@ -19,6 +19,9 @@ for FILE in $IN_FILES; do
         M4="build/docs/$(echo "$TAG" | tr ':' '_' | tr -d '%').m4"
         MD="build/docs/$(echo "$TAG" | tr ':' '_' | tr -d '%').md"
 
+        # Add directive to include custom m4 macros
+        echo "include(\`macros.m4')" > "$M4"
+
         # Extract documentation context from TAG
         DOC_CTX=$(echo "$TAG" | cut -d ':' -f 2)
 
@@ -26,7 +29,7 @@ for FILE in $IN_FILES; do
         DOC_PAT="/$TAG/,/$END_MARKER/{/$TAG/!{/$END_MARKER/!p;};}"
 
         # Extract documentation into snippet files
-        sed -n "$DOC_PAT" "$FILE" | cut -c 4- > "$M4"
+        sed -n "$DOC_PAT" "$FILE" | cut -c 4- >> "$M4"
 
         # Expand dcoumentation markers into macros
         sed -i "s/__NAME__/__NAME__(<<<$DOC_CTX>>>)/g" "$M4"
@@ -38,5 +41,14 @@ for FILE in $IN_FILES; do
         sed -i "s/__GIVEN__/__GIVEN__(<<<$DOC_CTX>>>)/g" "$M4"
         sed -i "s/__WHEN__/__WHEN__(<<<$DOC_CTX>>>)/g" "$M4"
         sed -i "s/__THEN__/__THEN__(<<<$DOC_CTX>>>)/g" "$M4"
+
+        # Expand macros to generate markdown file
+        m4 -DMAN -I docs/ "$M4" > "$MD"
+
+        # Remove blank lines from top of MD
+        sed -i '/./,$!d' "$MD"
+
+        # Remove intermediate m4 file
+        rm -f "$M4"
     done
 done
