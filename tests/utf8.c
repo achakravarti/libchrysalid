1de3e14f8e2909aef4bbae9bcc64f7db16d9c312
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-03 15:52:26 +0530

    Define samples for testing
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ tests/utf8.c
@@ -0,0 +1,10 @@
+
+#define SAMPLE_EN_1     "Jim quickly realized that the beautiful gowns" \
+                        " are expensive."
+
+#define SAMPLE_EN_2     "Jackie will budget for the most expensive" \
+                        " zoology equipment."
+#define SAMPLE_EL_1     "Θέλει αρετή και τόλμη η ελευθερία. (Ανδρέας Κάλβος)."
+
+#define SAMPLE_EL_2     "Ο καλύμνιος σφουγγαράς ψιθύρισε πως θα βουτήξει" \
+                        " χωρίς να διστάζει."
