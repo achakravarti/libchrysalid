d8fa32d3eb0570436e2d3a6e8994023c5a444810
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-03 16:39:56 +0530

    Refactor declaration
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/utf8.h
+++ include/utf8.h
@@ -21,28 +21,30 @@ typedef char cy_utf8_t;
 
 
 /* Prototypes for factory functions  */
-extern cy_utf8_t *cy_utf8_new(const char *);
-extern cy_utf8_t *cy_utf8_copy(const cy_utf8_t *);
-extern cy_utf8_t *cy_utf8_clone(const cy_utf8_t *);
-extern void cy_utf8_free(cy_utf8_t **);
+extern CY_HOT CY_RSAFE cy_utf8_t    *cy_utf8_new(const char[static 1]);
+extern CY_HOT CY_RSAFE cy_utf8_t    *cy_utf8_copy(cy_utf8_t[static 1]);
+extern CY_HOT CY_RSAFE cy_utf8_t    *cy_utf8_clone(const cy_utf8_t[static 1]);
+extern CY_HOT void                   cy_utf8_free(cy_utf8_t *[static 1]);
 
 
 /* Prototype for comparator function  */
-extern enum cy_cmp cy_utf8_cmp(const cy_utf8_t *, const cy_utf8_t *);
+extern CY_HOT enum cy_cmp   cy_utf8_cmp(const cy_utf8_t[static 1],
+                                        const cy_utf8_t[static 1]);
 
 
 /* Prototypes for accessor functions  */
-extern size_t cy_utf8_len(const cy_utf8_t *);
-extern bool cy_utf8_match(const cy_utf8_t *);
+extern size_t   cy_utf8_len(const cy_utf8_t[static 1]);
+extern size_t   cy_utf8_refc(const cy_utf8_t[static 1]);
+extern bool     cy_utf8_match(const cy_utf8_t[static 1]);
 
 
-#define cy_utf8_lt(lhs, rhs) (cy_utf8_cmp(lhs, rhs) == CY_CMP_LT)
+#define cy_utf8_lt(lhs, rhs)    (cy_utf8_cmp(lhs, rhs) == CY_CMP_LT)
 
 
-#define cy_utf8_eq(lhs, rhs) (cy_utf8_cmp(lhs, rhs) == CY_CMP_EQ)
+#define cy_utf8_eq(lhs, rhs)    (cy_utf8_cmp(lhs, rhs) == CY_CMP_EQ)
 
 
-#define cy_utf8_gt(lhs, rhs) (cy_utf8_cmp(lhs, rhs) == CY_CMP_GT)
+#define cy_utf8_gt(lhs, rhs)    (cy_utf8_cmp(lhs, rhs) == CY_CMP_GT)
 
 
 /* C++ compatibility */
