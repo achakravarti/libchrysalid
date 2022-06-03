567faa072b52b1ae75c262effea2093c03d6b88f
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-03 16:39:41 +0530

    Plan out tests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tests/utf8.c
+++ tests/utf8.c
@@ -4,7 +4,58 @@
 
 #define SAMPLE_EN_2     "Jackie will budget for the most expensive" \
                         " zoology equipment."
+
 #define SAMPLE_EL_1     "Θέλει αρετή και τόλμη η ελευθερία. (Ανδρέας Κάλβος)."
 
 #define SAMPLE_EL_2     "Ο καλύμνιος σφουγγαράς ψιθύρισε πως θα βουτήξει" \
                         " χωρίς να διστάζει."
+
+// new empty string for null
+// new empty string for empty
+// new english panagram
+// new greek panagram
+
+
+// copy empty string (single)
+// copy english string (single)
+// copy greek string (single)
+// copy empty string (multiple)
+// copy english string (multiple)
+// copy greek string (multiple)
+
+
+// clone empty string (single)
+// clone english string (single)
+// clone greek string (single)
+// clone empty string (multiple)
+// clone english string (multiple)
+// clone greek string (multiple)
+
+
+// free single empty string
+// free single english string
+// free single greek string
+
+// free single copy empty string
+// free single copy english string
+// free single copy greek string
+
+// free multiple copy empty string
+// free multiple copy english string
+// free multiple copy greek string
+
+// free single clone empty string
+// free single clone english string
+// free single clone greek string
+
+// free multiple clone empty string
+// free multiple clone english string
+// free multiple clone greek string
+
+// match empty string empty
+// match empty string non-empty
+// match english string empty
+// match english string true
+// match english string false
+// match greek string true
+// match greek string false
