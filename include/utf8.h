7207f9ba7029e2385e49af4bb5b070a6d0783562
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-13 09:54:16 +0530

    Add documentation markers
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/utf8.h
+++ include/utf8.h
@@ -17,6 +17,15 @@ extern "C" {
 #endif
 
 
+/*
+ * __NAME__
+ *
+ * __SYNOPSIS__
+ *
+ * __DESCRIPTION__
+ *
+ * __NOTES__
+ */
 typedef char cy_utf8_t;
 
 
@@ -27,9 +36,27 @@ extern CY_HOT CY_RSAFE cy_utf8_t    *cy_utf8_clone(const cy_utf8_t[static 1]);
 extern CY_HOT void                   cy_utf8_t_free__(cy_utf8_t *[static 1]);
 
 
+/*
+ * __NAME__
+ *
+ * __SYNOPSIS__
+ *
+ * __DESCRIPTION__
+ *
+ * __NOTES__
+ */
 #define cy_utf8_new_empty()     cy_utf8_new("");
 
 
+/*
+ * __NAME__
+ *
+ * __SYNOPSIS__
+ *
+ * __DESCRIPTION__
+ *
+ * __NOTES__
+ */
 #define cy_utf8_free(ctx)   cy_utf8_t_free__(ctx)
 
 
@@ -38,12 +65,39 @@ extern CY_HOT enum cy_cmp   cy_utf8_cmp(const cy_utf8_t[static 1],
                                         const cy_utf8_t[static 1]);
 
 
+/*
+ * __NAME__
+ *
+ * __SYNOPSIS__
+ *
+ * __DESCRIPTION__
+ *
+ * __NOTES__
+ */
 #define cy_utf8_lt(lhs, rhs)    (cy_utf8_cmp(lhs, rhs) == CY_CMP_LT)
 
 
+/*
+ * __NAME__
+ *
+ * __SYNOPSIS__
+ *
+ * __DESCRIPTION__
+ *
+ * __NOTES__
+ */
 #define cy_utf8_eq(lhs, rhs)    (cy_utf8_cmp(lhs, rhs) == CY_CMP_EQ)
 
 
+/*
+ * __NAME__
+ *
+ * __SYNOPSIS__
+ *
+ * __DESCRIPTION__
+ *
+ * __NOTES__
+ */
 #define cy_utf8_gt(lhs, rhs)    (cy_utf8_cmp(lhs, rhs) == CY_CMP_GT)
 
 
