2c1f9b5d2704ef6c4b2993ae9bab815d7c53d319
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-15 09:20:38 +0530

    Load and dump sample JSON
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- examples/cjson.c
+++ examples/cjson.c
@@ -1,8 +1,10 @@
+#include "../include/utf8.h"
 #include "../external/cJSON/cJSON.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 
-const char *example = "{"
+const char *sample = "{"
           "\"name\": \"Awesome 4K\","
           "\"resolutions\": ["
           "  {"
@@ -21,10 +23,55 @@ const char *example = "{"
         "}";
 
 
+static cJSON *
+json_new(void)
+{
+       cJSON *ctx = cJSON_Parse(sample);
+
+       if (!ctx) {
+                printf("JSON parsing failed!\n");
+                abort();
+        }
+
+       return ctx;
+}
+
+
+static void
+json_free(cJSON **ctx)
+{
+        cJSON *j;
+
+        if (ctx && (j = *ctx))
+                cJSON_Delete(j);
+
+        *ctx = NULL;
+}
+
+
+static cy_utf8_t *
+json_print(cJSON *ctx, bool pretty)
+{
+        return cy_utf8_new(pretty
+                           ? cJSON_Print(ctx)
+                           : cJSON_PrintUnformatted(ctx));
+}
+
+
 int main(int argc, char *argv[static 1])
 {
         (void) argc;
         (void) argv;
 
+        cJSON *j = json_new();
+
+
+        CY_AUTO(cy_utf8_t) *pretty = json_print(j, true);
+        printf("%s\n", pretty);
+
+        CY_AUTO(cy_utf8_t) *raw = json_print(j, false);
+        printf("%s\n", raw);
+
+        json_free(&j);
         return EXIT_SUCCESS;
 }
