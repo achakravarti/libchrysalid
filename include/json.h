5ea112f52141416597a2df651f6f09d94b2400ef
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-16 08:50:56 +0530

    Declare JSON interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/json.h
+++ include/json.h
@@ -7,6 +7,9 @@
 #include "ext.h"
 #include "utf8.h"
 
+/* Standard library dependencies */
+#include <stdint.h>
+
 
 /* C++ compatiblity */
 #ifdef __cplusplus
@@ -14,7 +17,7 @@ extern "C" {
 #endif
 
 
-typedef void cy_json_t;
+typedef uintptr_t cy_json_t;
 
 enum cy_json_type {
         CY_JSON_TYPE_NULL,
@@ -26,35 +29,31 @@ enum cy_json_type {
 };
 
 
-typedef void (cy_json_itr_f)(const cy_json_t *, void *);
+typedef void (cy_json_itr_f)(const cy_json_t [static 1], void *);
 
 
-cy_json_t *cy_json_new(const char *src);
+/* Prototypes for factory functions */
+extern CY_HOT CY_RSAFE cy_json_t    *cy_json_new(const char [static 1]);
+extern CY_HOT CY_RSAFE cy_json_t    *cy_json_copy(const cy_json_t [static 1]);
+extern CY_HOT CY_RSAFE cy_json_t    *cy_json_clone(const cy_json_t [static 1]);
+extern CY_HOT void                   cy_json_t_free__(cy_json_t *[static 1]);
+
+#define cy_json_free(ctx)   cy_json_t_free__(ctx)
 
 
-void cy_json_t_free__(cy_json_t **ctx);
-
-
-cy_utf8_t *cy_json_print(const cy_json_t *ctx, bool pretty);
-
-
-bool cy_json_has(const cy_json_t *ctx, const char *key);
-
-
-const cy_json_t *cy_json_node(const cy_json_t *ctx, const char *key);
-
-
-enum cy_json_type cy_json_type(const cy_json_t *ctx);
-
-cy_utf8_t *cy_json_string(const cy_json_t *ctx);
-
-double cy_json_number(const cy_json_t *ctx);
-
-
-bool cy_json_bool(const cy_json_t *ctx);
-
-
-void cy_json_map(const cy_json_t *ctx, cy_json_itr_f *itr, void *opt);
+/* Prototypes for accessor functions */
+extern bool                          cy_json_has(const cy_json_t [static 1],
+                                                 const char [static 1]);
+extern CY_RSAFE const cy_json_t     *cy_json_get(const cy_json_t [static 1],
+                                                 const char [static 1]);
+extern enum cy_json_type             cy_json_type(const cy_json_t [static 1]);
+extern CY_RSAFE cy_utf8_t           *cy_json_string(const cy_json_t [static 1]);
+extern double                        cy_json_number(const cy_json_t [static 1]);
+extern bool                          cy_json_bool(const cy_json_t [static 1]);
+extern CY_RSAFE cy_utf8_t           *cy_json_print(const cy_json_t [static 1],
+                                                   bool);
+extern void                          cy_json_map(const cy_json_t [static 1],
+                                                 cy_json_itr_f *, void *);
 
 
 /* C++ compatibility */
