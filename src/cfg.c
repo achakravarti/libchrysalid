baf26190da68f1d2559cb0426e55588a194aadeb
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-20 09:48:03 +0530

    Add assertions
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/cfg.c
+++ src/cfg.c
@@ -1,6 +1,11 @@
+/* Libchrysalid dependencies */
 #include "../include/cfg.h"
 
+/* External dependencies */
 #include "../external/iniparser/src/iniparser.h"
+
+/* Standard library dependencies */
+#include <assert.h>
 
 
 struct cy_cfg__ {
@@ -12,28 +17,28 @@ struct cy_cfg__ {
 cy_cfg_t *
 cy_cfg_new(const char *path)
 {
-
+        assert(path);
+        assert(*path);
 }
 
 
 cy_cfg_t *
 cy_cfg_copy(cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
 
 
 cy_cfg_t *
 cy_cfg_clone(const cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
 
 
 void
 cy_cfg_free__(cy_cfg_t **ctx)
 {
-
 }
 
 
@@ -41,33 +46,33 @@ cy_cfg_free__(cy_cfg_t **ctx)
 bool
 cy_cfg_bool(const cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
 
 
 long
 cy_cfg_int(const cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
 
 
 double
 cy_cfg_flt(const cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
 
 
 const char *
 cy_cfg_str(const cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
 
 
 cy_utf8_t *
 cy_cfg_print(const cy_cfg_t *ctx)
 {
-
+        assert(ctx);
 }
