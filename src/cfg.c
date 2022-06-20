0cb2e566461c7693f8527e4a265d43d72895d0e1
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-20 11:12:43 +0530

    Implement factory functions
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/cfg.c
+++ src/cfg.c
@@ -1,5 +1,6 @@
 /* Libchrysalid dependencies */
 #include "../include/cfg.h"
+#include "../include/hptr.h"
 
 /* External dependencies */
 #include "../external/iniparser/src/iniparser.h"
@@ -19,6 +20,17 @@ cy_cfg_new(const char *path)
 {
         assert(path);
         assert(*path);
+
+        cy_cfg_t *ctx = cy_hptr_new(sizeof *ctx);
+        ctx->path = cy_utf8_new(path);
+        ctx->hnd = iniparser_load(path);
+
+        if (CY_UNLIKELY(!ctx->hnd)) {
+                printf("Failed to load config file \"%s\"!\n", path);
+                abort();
+        }
+
+        return ctx;
 }
 
 
@@ -26,6 +38,8 @@ cy_cfg_t *
 cy_cfg_copy(cy_cfg_t *ctx)
 {
         assert(ctx);
+
+        return cy_hptr_copy((cy_hptr_t *) ctx);
 }
 
 
@@ -33,14 +47,27 @@ cy_cfg_t *
 cy_cfg_clone(const cy_cfg_t *ctx)
 {
         assert(ctx);
+
+        return cy_cfg_new(ctx->path);
 }
 
 
 void
 cy_cfg_free__(cy_cfg_t **ctx)
 {
-}
+        cy_cfg_t *c;
 
+        if (CY_LIKELY(ctx && (c = *ctx))) {
+                if (cy_hptr_refc((cy_hptr_t *) c) == 1) {
+                        cy_utf8_free(&c->path);
+
+                        if (CY_LIKELY(c->hnd))
+                                iniparser_freedict(c->hnd);
+                }
+        }
+
+        cy_hptr_free((cy_hptr_t **) ctx);
+}
 
 
 bool
