f695dc10ca925f6c8cbd50523a9d7afec7f1dc91
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-20 09:14:12 +0530

    Declare config interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/cfg.h
+++ include/cfg.h
@@ -3,6 +3,8 @@
 #define __LIBCHRYSALID_INCLUDE_CFG_H__
 
 
+#include "utf8.h"
+
 
 
 /* C++ compatiblity */
@@ -14,6 +16,23 @@ extern "C" {
 /* Types */
 
 typedef struct cy_cfg__     cy_cfg_t;
+
+
+/* Prototypes for factory functions */
+extern cy_cfg_t *cy_cfg_new(const char *);
+extern cy_cfg_t *cy_cfg_copy(cy_cfg_t *);
+extern cy_cfg_t *cy_cfg_clone(const cy_cfg_t *);
+extern void cy_cfg_free__(cy_cfg_t **);
+
+#define cy_cfg_free(ctx) cy_cfg_free__(ctx)
+
+/* Prototypes for accessor functions */
+bool cy_cfg_bool(const cy_cfg_t *);
+long cy_cfg_int(const cy_cfg_t *);
+double cy_cfg_flt(const cy_cfg_t *t);
+const char *cy_cfg_str(const cy_cfg_t *);
+
+extern cy_utf8_t *cy_cfg_print(const cy_cfg_t *);
 
 
 /* C++ compatibility */
