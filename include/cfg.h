0718291791a7b14e9052d936ff7b80d66992b943
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-20 09:33:13 +0530

    Start implementing interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/cfg.h
+++ include/cfg.h
@@ -19,20 +19,38 @@ typedef struct cy_cfg__     cy_cfg_t;
 
 
 /* Prototypes for factory functions */
-extern CY_SAFE cy_cfg_t     *cy_cfg_new(const char *);
-extern CY_SAFE cy_cfg_t     *cy_cfg_copy(cy_cfg_t *);
-extern CY_SAFE cy_cfg_t     *cy_cfg_clone(const cy_cfg_t *);
-extern void                  cy_cfg_free__(cy_cfg_t **);
+
+extern CY_SAFE cy_cfg_t  *
+cy_cfg_new(const char *);
+
+extern CY_SAFE cy_cfg_t *
+cy_cfg_copy(cy_cfg_t *);
+
+extern CY_SAFE cy_cfg_t *
+cy_cfg_clone(const cy_cfg_t *);
+
+extern void
+cy_cfg_free__(cy_cfg_t **);
 
 #define cy_cfg_free(ctx)    cy_cfg_free__(ctx)
 
 
 /* Prototypes for accessor functions */
-extern CY_PSAFE bool            cy_cfg_bool(const cy_cfg_t *);
-extern CY_PSAFE long            cy_cfg_int(const cy_cfg_t *);
-extern CY_PSAFE double          cy_cfg_flt(const cy_cfg_t *t);
-extern CY_SAFE const char      *cy_cfg_str(const cy_cfg_t *);
-extern CY_SAFE cy_utf8_t       *cy_cfg_print(const cy_cfg_t *);
+
+extern CY_PSAFE bool
+cy_cfg_bool(const cy_cfg_t *);
+
+extern CY_PSAFE long
+cy_cfg_int(const cy_cfg_t *);
+
+extern CY_PSAFE double
+cy_cfg_flt(const cy_cfg_t *t);
+
+extern CY_SAFE const char *
+cy_cfg_str(const cy_cfg_t *);
+
+extern CY_SAFE cy_utf8_t *
+cy_cfg_print(const cy_cfg_t *);
 
 
 /* C++ compatibility */
--- src/cfg.c
+++ src/cfg.c
@@ -4,5 +4,70 @@
 
 
 struct cy_cfg__ {
-        dictionary *hnd;
+        cy_utf8_t       *path;
+        dictionary      *hnd;
 };
+
+
+cy_cfg_t *
+cy_cfg_new(const char *path)
+{
+
+}
+
+
+cy_cfg_t *
+cy_cfg_copy(cy_cfg_t *ctx)
+{
+
+}
+
+
+cy_cfg_t *
+cy_cfg_clone(const cy_cfg_t *ctx)
+{
+
+}
+
+
+void
+cy_cfg_free__(cy_cfg_t **ctx)
+{
+
+}
+
+
+
+bool
+cy_cfg_bool(const cy_cfg_t *ctx)
+{
+
+}
+
+
+long
+cy_cfg_int(const cy_cfg_t *ctx)
+{
+
+}
+
+
+double
+cy_cfg_flt(const cy_cfg_t *ctx)
+{
+
+}
+
+
+const char *
+cy_cfg_str(const cy_cfg_t *ctx)
+{
+
+}
+
+
+cy_utf8_t *
+cy_cfg_print(const cy_cfg_t *ctx)
+{
+
+}
